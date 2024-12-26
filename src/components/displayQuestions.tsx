3dc0ecb205fed84c0c915192b7c31045a819a870
itsMikeLowrey <32291941+itsMikeLowrey@users.noreply.github.com>
2024-12-25 23:36:59 -0800

    Fix lint Issue

--- src/components/displayQuestions.tsx
+++ src/components/displayQuestions.tsx
@@ -1,4 +1,3 @@
-// @ts-nocheck
 import { Box, Text } from "@chakra-ui/react";
 
 interface Message {
@@ -11,28 +10,36 @@ interface ChildProps {
   articlePage: string;
 }
 
+function getArray(lastMessage) {
+  if (typeof lastMessage === "string") {
+    return JSON.parse(lastMessage);
+  }
+  return lastMessage;
+}
+
 const Display: React.FC<ChildProps> = ({ messages, articlePage }) => {
   const lastMessage = messages[6];
   console.log(messages[6]["content"]);
+  const value = getArray(lastMessage);
   return (
     <div>
       <Box w="100%" p="1rem">
         <Text fontSize="3xl" color={"#00283A"} fontWeight="semibold">
-          Here are 3 statements from the article: here
+          Here are 3 statements from the article:
           <a href={articlePage} target="_blank">
             here
           </a>
           . You can play 2 truths and a lie with your students.
         </Text>
-        {lastMessage["content"].map((item) => (
-          <>
+        {value["content"].map((item, statement) => (
+          <div key={statement}>
             <Text fontSize="2xl" color={"#00283A"} fontWeight="">
               Statment: {item.statement}
             </Text>
             <Text fontSize="md" color={"#00283A"} fontWeight="">
               This Statement is: {item.truth.toString()}
             </Text>
-          </>
+          </div>
         ))}
       </Box>
     </div>
